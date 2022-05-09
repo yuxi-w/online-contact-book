f6912b1f62743c4a3e27c9b7d69ae5d2c5740336
Yuxi W <48072303+yuxi-w@users.noreply.github.com>
2022-05-09 16:41:48 -0400

    Update models and config

---
+++ src/main/groovy/ocb/GlobalConfig.groovy
@@ -0,0 +1,13 @@
+package ocb
+
+class GlobalConfig {
+    public static final def USER_TYPE = [
+            ADMINISTRATOR: "ADMINISTRATOR",
+            REGULAR_MEMBER: "REGULAR_MEMBER",
+    ]
+
+
+    public static Integer itemsPerPage() {
+        return 5
+    }
+}
